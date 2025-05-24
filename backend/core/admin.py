4dbc75ae37e7eacd56eb3a9a2a890fb2510401b2
Anes-Ae <AnesDev@gmail.com>
2025-05-24 15:08:16 +0330

    add app core for user managment

---
+++ backend/core/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import CustomUser
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    fieldsets = UserAdmin.fieldsets
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields':('email',)}),
+    )
